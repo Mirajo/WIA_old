1e36718f5ee2ad094ba62a43caab1c4359fa1917
Mirajo <mmj_chicago@yahoo.com>
2014-05-04 20:38:22 -0400

    Modernizer
    Initial

---
+++ Wk1/Modernizer/example1.js
@@ -0,0 +1,15 @@
+if(Modernizer.canvas){
+	//Canvas is supported
+	
+	var theCanvas = document.getElementById("Canvas1");
+	var ctx = theCanvas.getContext("2d");
+	
+	//Draw some text on canvas
+	ctx.font = "25pt Georgia";
+	ctx.fillText("Canvas is supported", 20, 60);
+	
+	}else {
+		//Canvas is not supported
+		//Polyfill would go here
+	}
+	console.log(Modernizer);
